ce6d7e29177dbd3db1f2b881a4816d871b5a6190
Valenciasebas4 <valenciasebastian80@gmail.com>
2023-11-11 18:08:19 -0500

    Create new project Concert, create folder DAL, Entities and class Ticket, create class DataBasecontext

---
+++ Concert/Concert/DAL/DataBaseContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial3_LondonoValenciaSebastian.DAL.Entities;
+using System.Diagnostics.Metrics;
+
+namespace Parcial3_LondonoValenciaSebastian.DAL
+{
+    public class DataBaseContext :DbContext
+    {
+
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+        // Se mapea la identidad para convertirla en una tabla
+        public DbSet<Ticket> Tickets { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            for (int i = 0; i < 30; i++)
+            {
+                modelBuilder.Entity<Ticket>().HasData(
+                    new Ticket
+                    {
+                        Id = Guid.NewGuid(),
+                        UseDate = null,
+                        IsUsed = false,
+                        EntranceGate = null
+                    }
+                );
+            }
+        }
+
+
+
+    }
+}
---
+++ Concert/Concert/DAL/Entities/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial3_LondonoValenciaSebastian.DAL.Entities
+{
+    public class Ticket
+    {
+        #region Properties
+        [Key] //PROPERTY KEY
+        public virtual Guid Id { get; set; }
+
+        [Display(Name = "Fecha de Uso")]
+        public virtual DateTime? UseDate { get; set; }
+
+        [Display(Name = "Se uso?")]
+        public bool? IsUsed { get; set; }
+
+        [Display(Name = "Porteria de Entrada")]
+        [MaxLength(500, ErrorMessage = "El campo {0} debe ser de {1} caracteres.")]
+        public string? EntranceGate { get; set; }
+        #endregion
+    }
+}
